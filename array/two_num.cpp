4b605b9e12f40e7599d253e522e04ee3f91d25bf
eyeboy <eyeboyplus@163.com>
2019-01-24 15:56:11 +0800

    cmake support

---
+++ array/two_num.cpp
@@ -0,0 +1,9 @@
+//
+// Created by eyeboy on 2019/1/24.
+//
+#include "./../lc.h"
+
+int main(int argc, char **argv) {
+
+    cout << "done.";
+}
---
+++ lc.h
@@ -0,0 +1,14 @@
+//
+// Created by eyeboy on 2019/1/24.
+//
+
+#ifndef LC_LC_H
+#define LC_LC_H
+
+#include <unordered_map>
+#include <vector>
+#include <iostream>
+
+using namespace std;
+
+#endif //LC_LC_H
